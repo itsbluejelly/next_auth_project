f24868bdd79bd71477fcf25d0f3d7e357f9a34ec
itsbluejelly <ronniedenzel0@gmail.com>
2024-01-24 20:32:43 +0300

    setup of zod schema

--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -3,6 +3,40 @@
 import { Button, Checkbox, Input, Link } from "@nextui-org/react"
 import {UserIcon, EnvelopeIcon, KeyIcon, PhoneIcon, EyeIcon, EyeSlashIcon} from "@heroicons/react/20/solid" 
 import React from "react"
+import {z} from "zod"
+import validator from "validator"
+
+const FormSchema = z.object({
+  firstName: z
+    .string()
+    .min(2, "First name has minimum of 2 characters")
+    .max(45, "First name has maximum of 45 characters")
+    .regex(new RegExp("^[a-zA-Z]+$", "No special characters allowed")),
+
+  lastName: z
+    .string()
+    .min(2, "First name has minimum of 2 characters")
+    .max(45, "First name has maximum of 45 characters")
+    .regex(new RegExp("^[a-zA-Z]+$", "No special characters allowed")),
+
+  email: z.string().email("Please enter a valid email"),
+  phone: z.string().refine(validator.isMobilePhone, "Please enter valid phone number"),
+  
+  password: z
+    .string()
+    .min(6, "Password has minimum of 6 characters")
+    .max(50, "Password has maximum of 50 characters"),
+
+  confirmPassword: z
+    .string()
+    .min(6, "Password has minimum of 6 characters")
+    .max(50, "Password has maximum of 50 characters"),
+
+  accepted: z.literal(true, { errorMap: () => ({ message: "Please accept all terms" })})
+}).refine(data => data.password === data.confirmPassword, { 
+  message: "Password and confirm password don't match!",
+  path: ["password", "confirmPassword"] 
+})
 
 function SignUpForm() {
   const [isVisiblePass, setIsVisiblePass] = React.useState<boolean>(false)
