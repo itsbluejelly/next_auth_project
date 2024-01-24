4243b557a88fe584787aceaacf0d3c107fa3790e
itsbluejelly <ronniedenzel0@gmail.com>
2024-01-25 02:59:22 +0300

    suggestion by stackoverflow

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,60 @@
+import prisma from "@/src/lib/prisma"
+import {AuthOptions} from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcrypt"
+import { User } from "@prisma/client"
+
+export const authOptions: AuthOptions = {
+    pages: { signIn: "/auth/signIn" },
+
+    providers: [CredentialsProvider({ 
+        name: "Credentials",
+
+        credentials: {
+            username: { 
+                label: "User Name",
+                type: "text",
+                placeholder: "Your User Name"
+            },
+
+            password : { 
+                label: "Password",
+                type: "password"
+            }
+        },
+
+        async authorize(credentials){
+            const user = await prisma.user.findUnique({ where: { email: credentials?.username }})
+
+            if(!user){
+                throw new Error("User name or password is invalid ")
+            }else if(!credentials?.password){
+                throw new Error("Please provide the password")  
+            }
+
+            const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+
+            if(!isPasswordCorrect){
+                throw new Error("Please provide the right password or username")
+            }
+
+            const {password, ...otherDetails} = user
+            return otherDetails
+        }
+    })],
+
+    callbacks: {
+        async jwt({token, user}){
+            if(user){
+                token.user = user as User
+            }
+            
+            return token
+        },
+
+        async session({token, session}){
+            session.user = token.user
+            return session
+        }
+    }
+}
